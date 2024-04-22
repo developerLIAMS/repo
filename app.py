17bc91644f28c9b26add6ce3e6371e0c473a3989
developerLIAMS <167822094+developerLIAMS@users.noreply.github.com>
2024-04-22 19:06:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,24 +20,45 @@ def obtener_tasa_euro_usd():
     tasa_venta = tasa_base * 1.02    # Venta a +2%
     return tasa_compra, tasa_venta  # Devuelve sin redondear
 
-def main():
+def obtener_dolar_blue():
+    url = 'https://criptoya.com/api/dolar'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        dolar_blue_ask = data['blue']['ask']
+        tasa_compra = dolar_blue_ask - 25
+        tasa_venta = dolar_blue_ask + 10
+        return dolar_blue_ask, tasa_compra, tasa_venta
+    else:
+        return None, None, None
+
+def mostrar_tasas():
+    st.title("Consulta de Tasas de Cambio")
     monedas_enviar_recibir = ['cop', 'brl', 'pen', 'clp']
     monedas_solo_enviar = ['mxn', 'pyg', 'uyu']
+    resultados = {}
+    
+    for moneda in monedas_enviar_recibir:
+        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
+        tasa_recibir = obtener_tasa('usdt', moneda, "recibir")
+        resultados[moneda] = f"{tasa_enviar} / {tasa_recibir}"
 
-    st.title("Consulta de Tasas de Cambio")
-    selected_coin = st.selectbox("Elige la moneda", monedas_enviar_recibir + monedas_solo_enviar)
-    tipo = st.selectbox("Elige el tipo de transacción", ["enviar", "recibir"])
-    if st.button("Obtener Tasa"):
-        tasa = obtener_tasa('usdt', selected_coin, tipo)
-        if tasa:
-            st.success(f"La tasa para {selected_coin} es {tasa}")
-        else:
-            st.error("Error al obtener la tasa.")
+    for moneda in monedas_solo_enviar:
+        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
+        resultados[moneda] = f"{tasa_enviar}"
 
-    # Mostrar tasa de cambio Euro a Dólar
-    if st.button("Tasa de Cambio EUR/USD"):
-        tasa_compra, tasa_venta = obtener_tasa_euro_usd()
-        st.write(f"Tasa de cambio EUR/USD - Compra: {tasa_compra}, Venta: {tasa_venta}")
+    tasa_compra, tasa_venta = obtener_tasa_euro_usd()
+    resultados["EUR/USD"] = f"Compra: {tasa_compra}, Venta: {tasa_venta}"
+
+    dolar_blue_ask, dolar_blue_compra, dolar_blue_venta = obtener_dolar_blue()
+    if dolar_blue_ask is not None:
+        resultados["Dólar Blue"] = f"Tasa Dólar Blue: {dolar_blue_ask}, Compra: {dolar_blue_compra}, Venta: {dolar_blue_venta}"
+    else:
+        resultados["Dólar Blue"] = "No disponible"
+
+    for key, value in resultados.items():
+        st.write(f"{key}: {value}")
 
 if __name__ == "__main__":
-    main()
+    if st.button('RUN'):
+        mostrar_tasas()
