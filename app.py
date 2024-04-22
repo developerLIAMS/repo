9ab19866b5103613a87c1794cd0afde3fe6b86d6
developerLIAMS <167822094+developerLIAMS@users.noreply.github.com>
2024-04-22 19:11:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
-import streamlit as st
 import requests
-from requests import get as requests_get
+import streamlit as st
 
+def obtener_tasa(coin, fiat, tipo):
+    url = f'https://criptoya.com/api/binancep2p/{coin}/{fiat}/0.1'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        ask = data['ask']
+        if tipo == "enviar":
+            return round(ask * 0.94, 1)  # Restar 6% y redondear a un decimal
+        elif tipo == "recibir":
+            return round(ask * 1.05, 1)  # Sumar 5% y redondear a un decimal
+    else:
+        return None
 
-# Función para obtener las tasas de cambio
-def obtener_tasas_de_cambio():
-    tasas = {}
-    monedas_enviar_recibir = ['cop', 'brl', 'pen', 'clp']
-    monedas_solo_enviar = ['mxn', 'pyg', 'uyu']
-    for moneda in monedas_enviar_recibir:
-        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
-        tasa_recibir = obtener_tasa('usdt', moneda, "recibir")
-        tasas[moneda.upper()] = (tasa_enviar, tasa_recibir)
-    for moneda in monedas_solo_enviar:
-        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
-        tasas[moneda.upper()] = tasa_enviar
-    return tasas
-
-# Función para obtener la tasa de cambio EUR/USD
 def obtener_tasa_euro_usd():
     tasa_base = 1.065  # Tasa EUR/USD
     tasa_compra = tasa_base * 0.995  # Compra a -0.5%
     tasa_venta = tasa_base * 1.02    # Venta a +2%
     return tasa_compra, tasa_venta  # Devuelve sin redondear
 
-# Función para obtener la tasa del dólar blue
 def obtener_dolar_blue():
     url = 'https://criptoya.com/api/dolar'
     response = requests.get(url)
@@ -37,26 +32,39 @@ def obtener_dolar_blue():
     else:
         return None, None, None
 
-# Configuración de la apariencia de la aplicación
-st.set_page_config(page_title="Consulta de Tasas de Cambio", page_icon=":money_with_wings:")
-st.title("Tasas de Cambio")
+def obtener_tasas_de_cambio():
+    tasas = []
+    
+    monedas_enviar_recibir = ['cop', 'brl', 'pen', 'clp']
+    monedas_solo_enviar = ['mxn', 'pyg', 'uyu']
 
-# Obtener y mostrar las tasas de cambio
-tasas = obtener_tasas_de_cambio()
-for moneda, tasa in tasas.items():
-    if isinstance(tasa, tuple):
-        st.write(f"{moneda}: {tasa[0]} / {tasa[1]}")
+    # Mostrar tasas para enviar y recibir
+    for moneda in monedas_enviar_recibir:
+        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
+        tasa_recibir = obtener_tasa('usdt', moneda, "recibir")
+        tasas.append((moneda.upper(), f"{tasa_enviar} / {tasa_recibir}"))
+
+    # Mostrar tasas solo para enviar
+    for moneda in monedas_solo_enviar:
+        tasa_enviar = obtener_tasa('usdt', moneda, "enviar")
+        tasas.append((moneda.upper(), str(tasa_enviar)))
+
+    # Mostrar tasa de cambio Euro a Dólar con compra y venta
+    tasa_compra, tasa_venta = obtener_tasa_euro_usd()
+    tasas.append(("EUR/USD", f"Compra: {tasa_compra}, Venta: {tasa_venta}"))
+
+    # Obtener y mostrar tasas del dólar blue
+    dolar_blue_ask, dolar_blue_compra, dolar_blue_venta = obtener_dolar_blue()
+    if dolar_blue_ask is not None:
+        tasas.append(("Dólar Blue", f"Tasa Dólar Blue: {dolar_blue_ask}, Compra: {dolar_blue_compra}, Venta: {dolar_blue_venta}"))
     else:
-        st.write(f"{moneda}: {tasa}")
+        tasas.append(("Dólar Blue", "No se pudo obtener la información del dólar blue."))
+    
+    return tasas
 
-# Mostrar la tasa de cambio EUR/USD
-tasa_compra, tasa_venta = obtener_tasa_euro_usd()
-st.write(f"EUR/USD: Compra: {tasa_compra}, Venta: {tasa_venta}")
-
-# Mostrar la tasa del dólar blue
-dolar_blue_ask, dolar_blue_compra, dolar_blue_venta = obtener_dolar_blue()
-if dolar_blue_ask is not None:
-    st.write(f"Dólar Blue: Tasa Dólar Blue: {dolar_blue_ask}, Compra: {dolar_blue_compra}, Venta: {dolar_blue_venta}")
-else:
-    st.write("No se pudo obtener la información del Dólar Blue")
+# Interfaz de usuario con Streamlit
+st.title("Consulta de Tasas de Cambio")
+tasas = obtener_tasas_de_cambio()
+for tasa in tasas:
+    st.write(f"{tasa[0]}: {tasa[1]}")
 
